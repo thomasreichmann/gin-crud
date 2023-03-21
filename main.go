493484876db638745344eace724bdf01d45af58a
thomasreichmann <thomasalmeidar@gmail.com>
2023-03-21 14:04:28 -0300

    base example

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ func setupRouter() *gin.Engine {
 	engine := gin.Default()
 
 	// Ping test
-	engine.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	engine.GET("/ping", func(context *gin.Context) {
+		context.String(http.StatusOK, "pong")
 	})
 
 	// Get user value
-	engine.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
+	engine.GET("/user/:name", func(context *gin.Context) {
+		user := context.Params.ByName("name")
 		value, ok := db[user]
 		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+			context.JSON(http.StatusOK, gin.H{"user": user, "value": value})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+			context.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
 		}
 	})
 
@@ -39,10 +39,19 @@ func setupRouter() *gin.Engine {
 	// TODO: try to create a router group with a relative path
 	// TODO: try to craete a router group with no middleware
 	// TODO: try to create a full crud mock group
+	// TODO: do a little more research into gin middlewares and handlerfuncs
 	authorized := engine.Group("/", gin.BasicAuth(gin.Accounts{
 		"foo":  "bar", // user:foo password:bar
 		"manu": "123", // user:manu password:123
 	}))
+
+	// Could be done like this too
+	//accounts := gin.Accounts{
+	//	"foo":  "bar",
+	//	"manu": "123",
+	//}
+	//authMiddleware := gin.BasicAuth(accounts)
+	//authorized := engine.Group("/", authMiddleware)
 
 	/* example curl for /admin with basicauth header
 	   Zm9vOmJhcg== is base64("foo:bar")
@@ -53,9 +62,9 @@ func setupRouter() *gin.Engine {
 	  	-H 'content-type: application/json' \
 	  	-d '{"value":"bar"}'
 	*/
-	authorized.POST("admin", func(c *gin.Context) {
+	authorized.POST("admin", func(context *gin.Context) {
 		// https://go.dev/ref/spec#Type_assertions
-		user := c.MustGet(gin.AuthUserKey).(string) // .(string) é "type assertion" ele garante que o resultado do MustGet não é nill e é uma 'string'
+		user := context.MustGet(gin.AuthUserKey).(string) // .(string) é "type assertion" ele garante que o resultado do MustGet não é nill e é uma 'string'
 
 		// Parse JSON
 		// TODO: try to add more values to this json object
@@ -63,9 +72,9 @@ func setupRouter() *gin.Engine {
 			Value string `json:"value" binding:"required"`
 		}
 
-		if c.Bind(&json) == nil {
+		if context.Bind(&json) == nil {
 			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+			context.JSON(http.StatusOK, gin.H{"status": "ok"})
 		}
 	})
 
